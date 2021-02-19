9b7a3f02fe436598e453222435836575e08fa862
Tom Bragg <tom.bragg.197@gmail.com>
2021-02-19 11:19:32 +0000

    QI-11 #comment Implemented read methods to ItemController and ItemDAO

--- src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
@@ -1,17 +1,47 @@
 package com.qa.ims.persistence.dao;
 
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Customer;
 import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.DBUtils;
 
 public class ItemDAO implements Dao<Item>{
+	
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	@Override
+	public Item modelFromResultSet(ResultSet resultSet) throws SQLException {
+		Long itemId = resultSet.getLong("item_id");
+		Long orderId = resultSet.getLong("order_id");
+		String itemName = resultSet.getString("item_name");
+		Float itemValue = resultSet.getFloat("item_value");
+		return new Item(itemId, orderId, itemName, itemValue);
+	}
 
 	@Override
 	public List<Item> readAll() {
-		// TODO Auto-generated method stub
-		return null;
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM items");) {
+			List<Item> items = new ArrayList<>();
+			while (resultSet.next()) {
+				items.add(modelFromResultSet(resultSet));
+			}
+			return items;
+		} catch (SQLException e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return new ArrayList<>();
 	}
 
 	@Override
@@ -36,12 +66,6 @@ public class ItemDAO implements Dao<Item>{
 	public int delete(long id) {
 		// TODO Auto-generated method stub
 		return 0;
-	}
-
-	@Override
-	public Item modelFromResultSet(ResultSet resultSet) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
