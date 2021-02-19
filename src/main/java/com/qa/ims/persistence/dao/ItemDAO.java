4ed0c3cc3ef22ee6d95ac448d006ec263423e3b5
Tom Bragg <tom.bragg.197@gmail.com>
2021-02-19 11:02:24 +0000

    QI-11 #comment Added ItemDAO and ItemController classes

---
+++ src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
@@ -0,0 +1,47 @@
+package com.qa.ims.persistence.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.qa.ims.persistence.domain.Item;
+
+public class ItemDAO implements Dao<Item>{
+
+	@Override
+	public List<Item> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item read(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item create(Item t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item update(Item t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete(long id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Item modelFromResultSet(ResultSet resultSet) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
